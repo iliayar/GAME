2ba710cdf5466c10bf9b9f9f3b87b8f9eb153b06
KuvshinovM <kuvshinovmark2001@gmail.com>
2018-05-17 15:37:09 +0300

    Testing unity with 'Visible Meta Files' mode

---
+++ Assets/Sсripts/WorkMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkMenu : MonoBehaviour {
+    public GameObject settings;
+    public void StartGame()
+    {
+        Application.LoadLevel("var_1");
+    }
+
+    public void Settings()
+    {
+        settings.SetActive(!settings.activeSelf);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
